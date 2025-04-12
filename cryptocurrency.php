d03a04d8f69d70a30ff8ea6ea9a79c4146488c03
payablelol <otsamed@outlook.com>
2025-04-12 19:25:24 +0800

    Create cryptocurrency.php

---
+++ cryptocurrency.php
@@ -0,0 +1,31 @@
+$array = [
+    [
+        [
+            [
+                "name" => "bitcoinwallet.com",
+                "description" => "这是一个官方的比特币钱包服务，自2014年以来已有近一百万用户使用。它提供免费的用户间比特币转账、个性化的比特币地址和区块链浏览器等功能。",
+                "github_link" => "https://github.com/EvolutionPHP/bitcoin-php" [^125^]
+            ],
+            [
+                "name" => "Guarda Wallet",
+                "description" => "这是一个安全便捷的在线加密货币钱包，支持比特币和其他资产。用户可以购买、质押、存储和交换加密货币、代币和稳定币。",
+                "github_link" => "https://github.com/GuardaWallet/guarda" [^129^]
+            ],
+            [
+                "name" => "Bitamp",
+                "description" => "这是一个开源的客户端免费比特币钱包，允许用户安全地在区块链上发送和接收比特币。",
+                "github_link" => "https://github.com/Bitamp/bitamp"
+            ],
+            [
+                "name" => "CEX.IO Wallet",
+                "description" => "这是一个在线比特币钱包，提供安全的用户资金保护，支持多种加密货币和支付选项。",
+                "github_link" => "https://github.com/CEXIO/cexio-wallet"
+            ],
+            [
+                "name" => "Paybis Wallet",
+                "description" => "这是一个免费且安全的在线比特币钱包，允许用户存储、发送和接收多种加密货币。",
+                "github_link" => "https://github.com/Paybis/paybis-wallet"
+            ]
+        ]
+    ]
+];
