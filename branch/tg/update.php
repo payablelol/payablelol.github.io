a4c312e2499379f4a1ae0c64608f4487651bf48e
payablelol <otsamed@outlook.com>
2025-04-25 08:31:55 +0800

    Update update.php

--- branch/tg/update.php
+++ branch/tg/update.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Telegram Bot Example whitout WebHook.
+ * Telegram Bot Example without WebHook.
  * It uses getUpdates Telegram's API.
  *
  * @author Gabriele Grillo <gabry.grillo@alice.it>
@@ -10,36 +10,58 @@ include 'Telegram.php';
 $bot_token = '7625094076:AAFzPQCV1l0wiMRiUlho0ushQdmE0Xr2SAY';
 $telegram = new Telegram($bot_token);
 
-// Get all the new updates and set the new correct update_id
-$req = $telegram->getUpdates();
-for ($i = 0; $i < $telegram->UpdateCount(); $i++) {
-    // You NEED to call serveUpdate before accessing the values of message in Telegram Class
-    $telegram->serveUpdate($i);
-    $text = $telegram->Text();
-    $chat_id = $telegram->ChatID();
+$last_update_id = 0;
+$gc_counter = 0;
 
-    if ($text == '/start') {
-        $reply = 'Working';
-        $content = ['chat_id' => $chat_id, 'text' => $reply];
-        $telegram->sendMessage($content);
+while (true) {
+    // 只获取新的消息
+    $params = [];
+    if ($last_update_id) {
+        $params['offset'] = $last_update_id + 1;
     }
-    if ($text == '/test') {
-        if ($telegram->messageFromGroup()) {
-            $reply = 'Chat Group';
-        } else {
-            $reply = 'Private Chat';
+    $req = $telegram->getUpdates($params);
+
+    $n = $telegram->UpdateCount();
+    for ($i = 0; $i < $n; $i++) {
+        $telegram->serveUpdate($i);
+        $text = $telegram->Text();
+        $chat_id = $telegram->ChatID();
+        $update_id = $telegram->UpdateID();
+
+        if ($update_id > $last_update_id) {
+            $last_update_id = $update_id;
         }
-        // Create option for the custom keyboard. Array of array string
-        $option = [['A', 'B'], ['C', 'D']];
-        // Get the keyboard
-        $keyb = $telegram->buildKeyBoard($option);
-        $content = ['chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $reply];
-        $telegram->sendMessage($content);
+
+        if ($text == '/start') {
+            $reply = 'Working';
+            $content = ['chat_id' => $chat_id, 'text' => $reply];
+            $telegram->sendMessage($content);
+        }
+        if ($text == '/test') {
+            if ($telegram->messageFromGroup()) {
+                $reply = 'Chat Group';
+            } else {
+                $reply = 'Private Chat';
+            }
+            $option = [['A', 'B'], ['C', 'D']];
+            $keyb = $telegram->buildKeyBoard($option);
+            $content = ['chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $reply];
+            $telegram->sendMessage($content);
+        }
+        if ($text == '/git') {
+            $reply = 'Check me on GitHub: https://github.com/Eleirbag89/TelegramBotPHP';
+            $content = ['chat_id' => $chat_id, 'text' => $reply];
+            $telegram->sendMessage($content);
+        }
     }
-    if ($text == '/git') {
-        $reply = 'Check me on GitHub: https://github.com/Eleirbag89/TelegramBotPHP';
-        // Build the reply array
-        $content = ['chat_id' => $chat_id, 'text' => $reply];
-        $telegram->sendMessage($content);
+
+    // 每次循环后休息1秒，降低资源消耗
+    sleep(1);
+
+    // 定期主动进行垃圾回收
+    $gc_counter++;
+    if ($gc_counter >= 60) {
+        gc_collect_cycles();
+        $gc_counter = 0;
     }
 }
