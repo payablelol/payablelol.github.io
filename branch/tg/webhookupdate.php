6e4b4b350f0d4603800424f4ff31ccbf7f8c1ae4
payablelol <otsamed@outlook.com>
2025-04-28 15:26:17 +0800

    Update webhookupdate.php

--- branch/tg/webhookupdate.php
+++ branch/tg/webhookupdate.php
@@ -8,18 +8,7 @@ include 'Telegram.php';
 
 // Set the bot TOKEN
 $bot_token = '7625094076:AAFzPQCV1l0wiMRiUlho0ushQdmE0Xr2SAY';
-$req = $telegram->getUpdates();
-
-for ($i = 0; $i < $telegram-> UpdateCount(); $i++) {
-	// You NEED to call serveUpdate before accessing the values of message in Telegram Class
-	$telegram->serveUpdate($i);
-	$text = $telegram->Text();
-	$chat_id = $telegram->ChatID();
-
-	if ($text == '/start') {
-		$reply = 'Working';
-		$content = array('chat_id' => $chat_id, 'text' => $reply);
-		$telegram->sendMessage($content);
-	}
-	// DO OTHER STUFF
-}
+$telegram = new Telegram($bot_token);
+$chat_id = $telegram->ChatID();
+$content = array('chat_id' => $chat_id, 'text' => 'Test');
+$telegram->sendMessage($content);
